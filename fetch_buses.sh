5c928236fbb0e48d668d422204936f6a31c7e584
Corey Rowe <coreyr1196@gmail.com>
2021-04-11 09:16:12 -0400

    Sanitize URL input

--- fetch_buses.sh
+++ fetch_buses.sh
@@ -3,8 +3,8 @@
 # Poll SMART API for location of all buses with vehicle IDs listed in buses_to_locate.txt
 while read bus_number; do
     echo "Fetching info for vehicle #$bus_number"
-    wget -q -O info_$bus_number.json https://www.smartbus.org/DesktopModules/Smart.Endpoint/proxy.ashx?method=predictionsforbus&vid=$bus_number
-    sleep 2
+    wget -q -O info_$bus_number.json "https://www.smartbus.org/DesktopModules/Smart.Endpoint/proxy.ashx?method=predictionsforbus&vid=$bus_number"
+	sleep 2
 done <buses_to_locate.txt
 
 # Combine all JSON responses into one file (as an array). jq must be installed.
